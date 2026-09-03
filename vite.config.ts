2e73176e9792584ab5de43a15740aa3bce033992
pedro7k-tech <mckevinmonstro@gmail.com>
2026-09-03 15:01:59 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
   server: {
     port: 3000,
     open: true
   },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
+    emptyOutDir: true,
     sourcemap: false
   }
 })
+
